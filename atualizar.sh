2acd8776ae4fe46ab22b7bc19127d43d73145ab3
nickpedro <43183915+nickpedro@users.noreply.github.com>
2019-01-17 16:33:41 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -6,24 +6,32 @@ echo
 	sudo apt update
 echo
 echo Listas atualizadas
+sleep 5
+clear
 
 echo Atualizando os Software
 echo
 	sudo apt -y upgrade
 echo
 echo Software atualizados
+sleep 5
+clear
 
 echo Atualizando o Kernel
 echo
 	sudo apt -y dist-upgrade
 echo
 echo Kernel atualizados
+sleep 5
+clear
 
 echo Removendo o Software 
 echo
 	sudo apt -y autoremove
 echo
 echo Software removidos
+sleep 5
+clear
 
 echo Limpando o Cache 
 	sudo apt -y autoclean
